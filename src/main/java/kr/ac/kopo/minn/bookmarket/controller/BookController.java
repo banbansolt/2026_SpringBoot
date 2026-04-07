40ef3a3c7cfef47489b1c2f82cc184d89619df19
AISW-509-204 <banbansolt@gmail.com>
2026-04-07 11:51:22 +0900

    5장 도서상세정보, 카테고리별도서 목록, 도서카테고리와 출판사가 일치하는 필터링 결과 도서목록 (Controller 미완성)

--- src/main/java/kr/ac/kopo/minn/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/minn/bookmarket/controller/BookController.java
@@ -6,20 +6,49 @@ import kr.ac.kopo.minn.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/books") // 클래스 전체의 기본 주소를 /books로 설정 (권장)
 public class BookController {
+
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    // 접속 주소: http://localhost:8080/books
+    @GetMapping // 클래스 수준의 /books 주소를 그대로 사용
     public String requestBookList(Model model){
         List<Book> listOfBooks = bookService.getAllBookList();
         model.addAttribute("bookList", listOfBooks);
         return "books";
+    }
+
+    // 접속 주소: http://localhost:8080/books/book?id=isbn1001
+    @GetMapping("/book")
+    public String requestBookById(@RequestParam("id") String bookId, Model model){
+        Book book = bookService.getBookById(bookId);
+        model.addAttribute("book", book);
+        return "book";
+    }
+
+    @GetMapping("/{category}")
+    public String requestBooksByCategory(@PathVariable("category") String bookCategory, Model model) {
+        List<Book> booksByCategory = bookService.getBookListByCategory(bookCategory);
+        model.addAttribute("bookList", booksByCategory);
+        return "books";
+    }
+
+
+    // 접속 주소: http://localhost:8080/books/all
+    @GetMapping("/all")
+    public ModelAndView requestAllBooks(){
+        ModelAndView modelAndView = new ModelAndView();
+        List<Book> list = bookService.getAllBookList();
+        modelAndView.addObject("bookList", list);
+        modelAndView.setViewName("books");
+        return modelAndView;
     }
 }
